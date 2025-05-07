39538bb7374bbd1c74765d3f8c91f8afecea7a2c
Telman Mammadov <tehsilelm20212@gmail.com>
2025-05-07 16:43:51 +0400

    add accordion text to compliance

--- src/pages/Compliance_And_Prioritization.tsx
+++ src/pages/Compliance_And_Prioritization.tsx
@@ -1,4 +1,5 @@
 import { ClipboardList, Briefcase, BarChart3, UsersRound } from "lucide-react";
+import ControlledAccordions from "../components/AccordionMui";
 
 const Compliance_And_Prioritization = () => {
     return (
@@ -80,6 +81,17 @@ const Compliance_And_Prioritization = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="text-center mt-20 mb-4">
+                <h1 className="text-2xl font-semibold mb-2">Prioritetləşdirmə metodologiyası</h1>
+                <p className="text-gray-600 text-sm md:text-base max-w-3xl mx-auto">
+                    Proqram çərçivəsində hər il müəyyən sayda şirkətlər iştirak üçün seçilərək proqrama dəvət olunacaq. Uyğun hesab edilən müraciətçilər iştiraklarının başlanmasından əvvəl rəsmi şəkildə məlumatlandırılacaq. Bu vaxt çərçivəsi, iştirakçılara ilkin müzakirələrin aparılması və zəruri hazırlıqların görülməsi üçün kifayət qədər imkan yaradır. Proqramın növbəti mərhələlərində digər şirkətlər də iştirak etmək imkanı əldə edəcəkdir.
+                </p>
+            </div>
+
+            <div className="max-w-2xl mx-auto mt-10 mb-10">
+                <ControlledAccordions />
             </div>
         </div>
     );
