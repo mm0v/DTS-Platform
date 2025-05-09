c872612ed1f18c4eb94844e5853edc9455341a0b
Telman Mammadov <tehsilelm20212@gmail.com>
2025-05-09 16:39:34 +0400

    video fix in mobile

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -2,12 +2,12 @@ const VideoSection = () => {
     return (
         <section className="w-full h-screen overflow-hidden">
             <video
-                className="w-full h-full object-cover"
+                className="w-full h-full object-contain sm:object-cover"
                 src="/video/video-section.mp4"
                 autoPlay
                 muted
                 loop
-                playsInline // required for iOS
+                playsInline
             />
         </section>
     );
