1cae70849e23e3ade0ac6d9bf2b943712ba6b274
Telman Mammadov <tehsilelm20212@gmail.com>
2025-05-30 10:54:43 +0400

    remove vite.confit...

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,8 @@
-// import { defineConfig } from "vite";
-// import tailwindcss from "@tailwindcss/vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// });
-
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://50.16.57.115:8080",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
-  },
 });
